da0bc406b5699651d34258eacacded85573eb2b3
timofeykl <84674136+timofeykl@users.noreply.github.com>
2021-05-23 20:02:19 +0300

    Plotting out

--- Nissan_analysis.py
+++ Nissan_analysis.py
@@ -2,52 +2,34 @@ import pandas as pd
 import cx_Oracle as cx
 import matplotlib.pyplot as plt
 
-dsn = """(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)
-(HOST=rnb-vmdwh-dbpr04.rci.renault.ru)(PORT=1521))
-(ADDRESS=(PROTOCOL=TCP)(HOST=rnb-vmdwh-dbpr03.rci.renault.ru)(PORT=1521))
-(LOAD_BALANCE= yes))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=DWHPR_DG)))
+dsn = """!!!
 """
-connection = cx.connect("iz01340", "iz01340", dsn)
+connection = cx.connect("!!!", dsn)
 
-print('fine')
-query = """
-select app_number,
-CREDIT_CONTRACT_NUM,
-PRODUCT_NAME,
-MODEL_CODE,
-INTEREST_RATE,
-EXPECTED_BANK_RATE,
-CAR_PRICE,
-LOAN_FOR_CAR_SUM,
-TOTAL_FINANCE_AMOUNT,
-credit_issue_date as credit_issue,
-credit_closed_date as credit_close,
-months_between(credit_closed_date,credit_issue_date) as fact_duration,
+# print('fine')
+# query = """
+# !!!
 
-duration,
+# duration,
 
-months_between(credit_closed_date,credit_issue_date)/duration AS fact_by_plan_duration,
-total_finance_amount/car_price as loan_by_price,
-Y_OF_FINANCING,
-M_OF_FINANCING
+# months_between(!!!)/!!! AS fact_by_plan_duration,
+# total_finance_amount/car_price as loan_by_price,
+# Y
+# M
 
-from dwh.v_indicator_fpa
-where brand='Nissan'
-and NEW_USED='NEW'
-and Y_OF_FINANCING in ('2019','2020')
-           """
+#            """
 
 df_ind = pd.read_sql(query, con=connection)
 print(1)
 
-df_sub = pd.read_excel('Nissan_sub2019-2020.xlsx')
+df_sub = pd.read_excel('!!!.xlsx')
 
 df_t = pd.merge(df_sub, df_ind, how='inner',
                 left_on='app_number', right_on='APP_NUMBER')
 
 print(df_t)
 
-df_t.to_excel('Subs2019-2020.xlsx', sheet_name='vin_sub')
+df_t.to_excel('!!!', sheet_name='vin_sub')
 
 df_corr = df_t.corr()
 
